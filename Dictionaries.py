296a8aad1a1cb335b5c856402d7adae95beb6681
alexander <alexandernyaga1999@gmail.com>
2023-12-06 15:38:37 +0300

    Import string

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,14 +1,4 @@
 import string
 
-def word_frequency(sentence):
-    sentence =sentence.translate(str.maketrans('','', string.punctuation))
-##split sentence into words.
-    words = sentence.split()
-    ##Count frequency of each word
-    frequency_dict = {}
-    for word in words:
-        frequency_dict[word] = frequency_dict.get(word, 0) + 1
 
-
-        return frequency_dict
 
